95fda4c8d7433c4b7751501f30352ae857da85a4
rukavain <magtoto599@gmail.com>
2025-04-03 16:32:29 +0800

    SEO image

--- app/lib/meta.ts
+++ app/lib/meta.ts
@@ -20,7 +20,7 @@ export const defaultMetadata: Metadata = {
     locale: "en-US",
     images: [
       {
-        url: "https://if-chatbot.vercel.app/screenshot.png",
+        url: "https://if-chatbot.vercel.app/image.png",
         alt: "if(chatbot) - The AI That Feels Like an If-Else Machine",
       },
     ],
